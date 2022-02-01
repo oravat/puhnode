a00134b1c220673dd9a7e4e41813976c14832a82
oravat <sixqqqq@gmail.com>
2022-02-01 11:32:16 +0200

    test

--- models/person.js
+++ models/person.js
@@ -12,8 +12,20 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{5,}/.test(v)
+        },
+      },
+      required: true
+    },
     id: Number,
 })
 
